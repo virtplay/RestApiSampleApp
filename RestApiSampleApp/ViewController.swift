255f4c47618e04d876b672a095db97a81750f0a0
Karthik B S <kokarbs@gmail.com>
2018-12-26 16:54:40 +0530

    Initial Commit

---
+++ RestApiSampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestApiSampleApp
+//
+//  Created by Karthik on 26/12/18.
+//  Copyright © 2018 k4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
